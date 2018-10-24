e816ebf0e9604f8714fc9c78c4f7bd0b2118decb
GeorgeCSNHS12 <soulstreme.maurag@gmail.com>
2018-10-24 15:36:34 +0800

    First Commit

--- hello_django_app/urls.py
+++ hello_django_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView
+from .views import HomePageView, AboutPageView, ContactPageView
 
 urlpatterns = [
     path('home/', HomePageView.as_view(), name="home_view"),
-    path('about/', AboutPageView.as_view(), name="about_view")
+    path('about/', AboutPageView.as_view(), name="about_view"),
+    path('contact/', ContactPageView.as_view(), name="contact_view")
 ]
--- hello_django_app/views.py
+++ hello_django_app/views.py
@@ -6,8 +6,14 @@ class HomePageView (TemplateView):
     template_name = "home.html"
 
     def get_context_data (self):
-        data = {"message_title" : "Yahoo!", "message" : "Django is Great!"}
+        data = {"title" : "Favorite Quote", 
+                "quote" : '"In Solitude, I Find My Answers"',
+                "anonymous" : "Kristen Butler"
+                }
         return data
 
 class AboutPageView (TemplateView):
     template_name = "about.html"
+
+class ContactPageView (TemplateView):
+    template_name = "contact.html"
